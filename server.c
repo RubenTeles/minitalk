3398a4550a13a2877b38c3f31492d8dea6e3e699
Ruben Teles <75394884+RubenTeles@users.noreply.github.com>
2022-02-15 23:26:07 +0000

    Add files via upload

--- server.c
+++ server.c
@@ -1,32 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_minitalk.c                                      :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/15 20:44:53 by rteles            #+#    #+#             */
-/*   Updated: 2022/02/15 23:05:04 by rteles           ###   ########.fr       */
+/*   Created: 2022/02/15 22:35:53 by rteles            #+#    #+#             */
+/*   Updated: 2022/02/15 23:25:24 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "ft_minitalk.h"
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-static void	handler(int signal)
+volatile sig_atomic_t	shutdown_flag = 1;
+
+void	cleanupRoutine(int signal_number)
 {
-	if (signal == SIGINT && write(1, "Bye bye...", 10))
-		exit(-1);
+    shutdown_flag = 0;
 }
 
 int	main(void)
 {
-	pid_t	ip;
+	pid_t				ip;
+	struct sigaction	signal_action;
 
-	signal(SIGINT, handler);
+	sigterm_action.sa_handler = &cleanupRoutine;
+	sigterm_action.sa_flags = 0;
+
 	ip = getpid();
 	printf("PID: %i", ip);
 	while (1)
@@ -35,9 +37,10 @@ int	main(void)
 }
 
 /*
-kill() - envia um sinal para um outro processo ou para um outro grupo de processos
-signal
-SIGINT       terminate process    interrupt program
-SIGUSR1 terminate process    User defined signal 1
-SIGUSR2 terminate process    User defined signal 2
-*/
+struct sigaction {
+               void     (*sa_handler)(int);
+               void     (*sa_sigaction)(int, siginfo_t *, void *);
+               sigset_t   sa_mask;
+               int        sa_flags;
+               void     (*sa_restorer)(void);
+           };*/
